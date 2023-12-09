f4ebf56a2e3dc366e53092af2757fee95129d298
Anup Kumar Sinha <anupk_sinha@yahoo.com>
2023-12-10 00:01:53 +0800

    created course-info-repository

--- course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResourse.java
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResourse.java
@@ -2,15 +2,13 @@ package com.pluralsight.courseinfo.server;
 
 import com.pluralsight.courseinfo.domain.Course;
 import com.pluralsight.courseinfo.repository.CourseRepository;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Comparator;
-import java.util.List;
+import java.util.stream.Stream;
 
 @Path("/courses")
 public class CourseResourse {
@@ -22,13 +20,18 @@ public class CourseResourse {
     }
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Course> getCourses(){
+    public Stream<Course> getCourses(){
         return courseRepository
                 .getAllCourses()
                 .stream()
-                .sorted(Comparator.comparing(Course::id))
-                .toList();
+                .sorted(Comparator.comparing(Course::id));
     }
-
-
+    @POST
+    @Path("/{id}/notes")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void AddNotes(@PathParam("id") String id, String notes)
+    {
+        courseRepository.addNotes(id, notes);
+    }
 }
+
--- course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
@@ -5,19 +5,47 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
+import java.util.logging.LogManager;
+
 public class CourseServer {
+    private static final Properties properties = new Properties();
     private static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
+
+    static {
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+        try {
+            InputStream propertiesStream = CourseServer.class.getResourceAsStream("/server.properties");
+
+            properties.load(propertiesStream);
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not Load BASE_URI.");
+        }
+    }
+
+
     private static final String BASE_URI = "http://localhost:8080/";
 
     public static void main(String[] args) {
-
-        LOG.info("Starting Http server!!");
-        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        String databaseFileName = LoadDatabaseFilename();
+       // String baseUri = LoadBaseUri();
+        LOG.info("Starting Http server with database {}", databaseFileName);
+        CourseRepository courseRepository = CourseRepository.openCourseRepository(databaseFileName);
         ResourceConfig config = new ResourceConfig().register(new CourseResourse(courseRepository));
         GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+    }
 
+    private static String LoadBaseUri() {
+            return properties.getProperty("base_uri");
+    }
 
+    private static String LoadDatabaseFilename() {
+            return properties.getProperty("course-info.database");
     }
 }
